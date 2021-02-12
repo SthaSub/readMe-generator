a58d5fd617768be77d87763c12ded272b3e9ae25
Subhash Shrestha <subhashshrestha3@gmail.com>
2021-02-12 14:42:31 +1030

    generating init and modified index.js

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["Enter project Title", "Enter project Description",
+ ,"Enter Installation Instruction", 
+ "Enter usage Information",
+"Enter contribution Guidelines",
+"Enter Test Instructions"," Enter your GitHub Username",
+"Enter Email Address",
+"Enter Table of COntents"];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return 
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
